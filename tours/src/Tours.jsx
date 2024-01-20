f00a99b6d6ed9d768170ade5a16030c0b50bce3d
somberlyx <lybasiddiqui02@gmail.com>
2024-01-20 16:15:41 +0400

    Rendered each tour onto the webpage

--- tours/src/Tours.jsx
+++ tours/src/Tours.jsx
@@ -1,6 +1,19 @@
 import Tour from "./Tour";
-const Tours = () => {
-  return <h2>tours component</h2>;
+
+const Tours = ({ tours }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>OUR TOURS</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
